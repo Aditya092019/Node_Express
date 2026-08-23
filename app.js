dd8588e4336f0e51d5e2480a748aa7f2333a9a90
Aditya Singh <aditya.singh@navyuginfo.com>
2026-08-23 18:23:17 +0530

    Update Express app

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const express = require('express');
 const app = express();
 
 app.use((req,res,next)=>{
-    console.log("Output from middleware 1");
+    console.log("Authentication middleware called");
     next();
 })
 
-app.use((req,res,next)=>{
-    console.log("Output from middleware 2");
+app.use('/welcome',(req,res,next)=>{
     console.log("Server is up and running on port 3000! Ready to handle requests.");
-    res.send('<h1>Hello World</h1>');
+    console.log("Access to the user who have valid credentials for party");
+    next();
+})
+
+app.get('/welcome',(req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    res.send('<h1>Welcome Guest</h1>');
 })
 
 app.listen(3000);
